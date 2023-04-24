4ba55a87dfd4b65819748150efc1784ee45d6afc
Hosein Danamehr <h.danamehr@trader4.net>
2023-04-24 13:22:46 +0330

    [FIX] fix config styles

--- src/Config/config.php
+++ src/Config/config.php
@@ -2,45 +2,39 @@
 
 use Werify\IdLaravel\Http\Controllers\Api\V1\AuthController;
 
-return
-	[
-		'version' => 'v1',
-		'app_key' => 'not_set',
-		'routes' =>
-		[
-			'group' => 'api/werify',
-			'request-otp' => '/request-otp',
-			'verify-otp' => '/verify-otp',
-			'qr' => 'qr',
-			'qr-image' => 'qr-image',
-			'qr-claim' => 'qr-claim'
-		],
-		'controllers' =>
-		[
-			'AuthController' =>
-			[
-				'class' => AuthController::class,
-				'request-otp' => 'requestOTP',
-				'verify-otp' => 'verifyOTP',
-				'qr' => 'qr',
-				'qr-image' => 'qrImage',
-				'qr-claim' => 'qrClaim'
-			],
-
-		],
-		'api' =>
-		[
-			'base_path' => 'https://id.werify.net',
-			'base_api_path' => 'https://id.werify.net/api',
-			'request-otp' => 'request-otp',
-			'verify-otp' => 'verify-otp',
-			'profile' => 'user/profile',
-			'profile-mobile-numbers' => 'user/profile/mobile-numbers',
-			'profile-metas' => 'user/profile/metas',
-			'profile-education' => 'user/profile/education',
-			'profile-financial-information' => 'user/profile/financial-information',
-			'qr' => 'qr',
-			'qr-image' => 'qr',
-			'qr-claim' => 'user/qr/'
-		]
-	];
+return [
+    'version' => 'v1',
+    'app_key' => 'not_set',
+    'routes' => [
+        'group' => 'api/werify',
+        'request-otp' => '/request-otp',
+        'verify-otp' => '/verify-otp',
+        'qr' => 'qr',
+        'qr-image' => 'qr-image',
+        'qr-claim' => 'qr-claim',
+    ],
+    'controllers' => [
+        'AuthController' => [
+            'class' => AuthController::class,
+            'request-otp' => 'requestOTP',
+            'verify-otp' => 'verifyOTP',
+            'qr' => 'qr',
+            'qr-image' => 'qrImage',
+            'qr-claim' => 'qrClaim',
+        ],
+    ],
+    'api' => [
+        'base_path' => 'https://id.werify.net',
+        'base_api_path' => 'https://id.werify.net/api',
+        'request-otp' => 'request-otp',
+        'verify-otp' => 'verify-otp',
+        'profile' => 'user/profile',
+        'profile-mobile-numbers' => 'user/profile/mobile-numbers',
+        'profile-metas' => 'user/profile/metas',
+        'profile-education' => 'user/profile/education',
+        'profile-financial-information' => 'user/profile/financial-information',
+        'qr' => 'qr',
+        'qr-image' => 'qr',
+        'qr-claim' => 'user/qr/',
+    ],
+];
